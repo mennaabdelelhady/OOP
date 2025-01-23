217c8e134fad3e9e7d030fb2a54b67bbb497b848
mennaabdelelhady <menamahmod660@gmail.com>
2025-01-24 00:42:05 +0200

    initial

---
+++ .history/src/main/java/oop/model/room/Customer_20250124003419.java
@@ -0,0 +1,6 @@
+package oop.model;
+
+public class Customer{
+    private LocalDate reservationStartDate;
+    private LocalDate reservationEndDate;
+}
---
+++ .history/src/main/java/oop/model/room/Customer_20250124003546.java
@@ -0,0 +1,6 @@
+package oop.model;
+
+public class Customer{
+    private String customerName;
+    private String customerId;
+}
